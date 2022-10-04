7db4db8b5781c0d40c9990759e38518a6622741d
chris Chen <59293007+githubucl@users.noreply.github.com>
2022-10-04 16:38:52 +1300

    cors

--- src/index.js
+++ src/index.js
@@ -15,6 +15,20 @@ const {
   removeUser,
 } = require("./utils/users");
 
+//Cors Configuration - Start
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested, Content-Type, Accept Authorization"
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "POST, PUT, PATCH, GET, DELETE");
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.use(cors());
 const io = new Server(server, {
   cors: {
